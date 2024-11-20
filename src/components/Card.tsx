f59e78d3511b13ccbc48e44c920a28dd6301ce8e
mykel68 <olowookeremicheal9568@gmail.com>
2024-11-20 10:27:09 +0100

    type config

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 interface CardProps {
     title: string
@@ -11,7 +12,7 @@ export default function Card({ title, description, image }: CardProps
     return (
         <Box>
             <div className="flex flex-col gap-2 items-start justify-center w-[15rem]">
-                <img src={image.src} alt={title} className='w-[7rem] aspect-square h-[7rem]' />
+                <Image src={image} alt={title} className='w-[7rem] aspect-square h-[7rem]' />
                 <h2 className='text-xl font-bold text-pretty tracking-wider'>{title}</h2>
                 <p className='text-[1rem] text-balance font-normal tracking-widest leading-4 '>{description}</p>
             </div>
---
+++ src/style/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        },
+        text: {
+            primary: '#ffffff',
+        },
+    },
+});
+
+export default theme;
